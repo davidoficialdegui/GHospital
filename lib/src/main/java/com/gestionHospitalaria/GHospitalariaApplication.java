929d6e30d1714e688757cea660922a4c44f3d65a
Jorge Cobián Vidal <jorgecobianvidal6@gmail.com>
2026-03-25 18:53:14 +0100

    Application creado

---
+++ lib/src/main/java/com/gestionHospitalaria/GHospitalariaApplication.java
@@ -0,0 +1,12 @@
+package com.gestionHospitalaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GHospitalariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GHospitalariaApplication.class, args);
+    }
+}
